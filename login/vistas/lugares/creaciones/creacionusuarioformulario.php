edfbca1f622c829a72e6cc196c673c5a91d199e8
aelogonpin <a_elogonpin@iestriana.com>
2023-03-03 18:52:11 +0100

    Quito la contraseña de uso

--- login/vistas/lugares/creaciones/creacionusuarioformulario.php
+++ login/vistas/lugares/creaciones/creacionusuarioformulario.php
@@ -163,7 +163,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'eloyrana@gmail.com';                     //SMTP username
-    $mail->Password   = 'jorzoajevwzdjbtk';                               //SMTP password
+    $mail->Password   = 'contraseña de app de google';                               //SMTP password
     $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 

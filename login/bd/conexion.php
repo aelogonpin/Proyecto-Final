c143b0c97fc12c3428d043897ac0144f58aa0503
Eloy González De Eiris Pineda <a_elogonpin@iestriana.com>
2023-06-23 10:00:15 +0200

    Update conexion.php

--- login/bd/conexion.php
+++ login/bd/conexion.php
@@ -1,8 +1,8 @@
 <?php 
 class Conexion{	  
     public static function Conectar() {        
-        define('servidor', '192.168.48.135');
-        define('nombre_bd', 'definitivo');
+        define('servidor', 'ipnotics.sytes.net');
+        define('nombre_bd', 'login_bd');
         define('usuario', 'root');
         define('password', 'Eloyto11#');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
